afcae429b78db8f80489adebfb3ffc383ff5e7c3
prabhu-madanagopal <prabhu.madanagopal@gmail.com>
2023-05-30 14:37:00 -0700

    add sk6812 foot print

--- src/footprints/diode.js
+++ src/footprints/diode.js
@@ -37,9 +37,6 @@ module.exports = {
         (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.to.str})
         (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.from.str})
         
-        ${''/* THT terminals */}
-        (pad 1 thru_hole rect (at -3.81 0 ${p.rot}) (size 1.778 1.778) (drill 0.9906) (layers *.Cu *.Mask) ${p.to.str})
-        (pad 2 thru_hole circle (at 3.81 0 ${p.rot}) (size 1.905 1.905) (drill 0.9906) (layers *.Cu *.Mask) ${p.from.str})
     )
   
     `
